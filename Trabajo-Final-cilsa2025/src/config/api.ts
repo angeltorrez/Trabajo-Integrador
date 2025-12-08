33c9a8584228edbc0c1ad392924cabfeb073b06b
Angel Torrez <angeltorrez113@gmail.com>
2025-12-07 21:15:07 -0300

    1

--- Trabajo-Final-cilsa2025/src/config/api.ts
+++ Trabajo-Final-cilsa2025/src/config/api.ts
@@ -1,6 +1,6 @@
 
 // En producción usar VITE_API_URL si está definida (https://vercel.com/angels-projects-9d35d66a/trabajo-integrador-xeec)
 // En desarrollo usar localhost:3001
-export const API_BASE_URL = "https://trabajo-integrador-1eu7.vercel.app/";
+export const API_BASE_URL = "https://trabajo-integrador-1eu7.vercel.app";
 
 export default API_BASE_URL;
